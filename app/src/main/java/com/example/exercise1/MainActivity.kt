e94034e80b3745d6087d4d9bbf1bca186f17b580
KarthiDreamr <karthidreamr@gmail.com>
2023-08-22 22:38:06 +0530

    Initial commit

---
+++ app/src/main/java/com/example/exercise1/MainActivity.kt
@@ -0,0 +1,72 @@
+package com.example.exercise1
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.exercise1.ui.theme.Exercise1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Exercise1Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(){
+    var tapCount by rememberSaveable { mutableStateOf(0) }
+    val context = LocalContext.current
+
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Tap Count: $tapCount")
+        Button(
+            onClick = {
+                tapCount++
+                Toast.makeText(context, "Button Tapped $tapCount times ! ", Toast.LENGTH_SHORT).show()
+            }
+        ) {
+            Text("Button")
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Exercise1Theme {
+        Greeting()
+    }
+}
