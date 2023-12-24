a2833f7d889ae86807632539ab42a67acfa61c00
KarthiDreamr <karthidreamr@gmail.com>
2023-12-24 15:37:13 +0530

    added screenshot

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,14 +9,14 @@ android {
 
     // manually added
 
-//    signingConfigs {
-//        release {
-//            storeFile file("keystore.jks")
-//            storePassword System.getenv("SIGNING_STORE_PASSWORD")
-//            keyAlias System.getenv("SIGNING_KEY_ALIAS")
-//            keyPassword System.getenv("SIGNING_KEY_PASSWORD")
-//        }
-//    }
+    signingConfigs {
+        create("release") {
+            storeFile = file("keystore.jks")
+            storePassword = System.getenv("SIGNING_STORE_PASSWORD")
+            keyAlias = System.getenv("SIGNING_KEY_ALIAS")
+            keyPassword = System.getenv("SIGNING_KEY_PASSWORD")
+        }
+    }
 
     defaultConfig {
         applicationId = "com.example.exercise1"
@@ -38,6 +38,7 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            signingConfig = signingConfigs.getByName("release")
         }
     }
     compileOptions {
